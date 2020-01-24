f1173b94d3128c1dd7a2611c8d0b70bf8d30203a
Keerthi Puthanaveedu <keerthi.puthan@gmail.com>
2020-01-24 06:56:18 -0500

    nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Nav1Component } from './nav1/nav1.component';
+import { Nav2Component } from './nav2/nav2.component';
+import { Nav3Component } from './nav3/nav3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Nav1Component,
+    Nav2Component,
+    Nav3Component
   ],
   imports: [
     BrowserModule,
